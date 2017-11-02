9e71992fe99e8114630e3b9b76bc25df4e703870
Dirk-Jan Faber <d.faber@gmail.com>
2017-11-02 16:20:46 +0100

    Update example.py
    Made the calculation correct.

--- example.py
+++ example.py
@@ -24,7 +24,7 @@ hx.set_reading_format("LSB", "MSB")
 # To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
-# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+# If 2000 grams is 184000 then 1 gram is 184000 / 2000 = 92.
 #hx.set_reference_unit(113)
 hx.set_reference_unit(92)
 
